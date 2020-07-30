18663835e46666e1aaf600b9f7eb60005d830364
Bangqiang <chi@MacBook-Pro-2.local>
2020-07-30 09:13:31 +0800

    code clean up

--- packages/viewer/babel.config.js
+++ packages/viewer/babel.config.js
@@ -3,22 +3,15 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: [
-      // [
-      //   '@babel/preset-env',
-      //   {
-      //     targets: {
-      //       browsers: ['last 2 Chrome versions']
-      //     }
-      //   }
-      // ]
-      // ['@babel/preset-typescript', { allowNamespaces: true }],
-      // '@babel/preset-react'
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            browsers: ['last 2 Chrome versions']
+          }
+        }
+      ]
     ],
-    plugins: [
-      // ['@babel/plugin-proposal-class-properties', { loose: true }],
-      // '@babel/plugin-proposal-nullish-coalescing-operator',
-      // '@babel/plugin-proposal-optional-chaining',
-      'react-hot-loader/babel'
-    ]
+    plugins: ['react-hot-loader/babel']
   };
 };
